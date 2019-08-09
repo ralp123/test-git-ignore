fe9ab61472396906b4a80d5f64094ed85aca5711
Ralp T Mancawan <rmancawan@rmancawan.innovuze>
2019-08-09 13:39:48 +0800

    Initial Commit

--- src/Numbers.js
+++ src/Numbers.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-class Clock extends React.Component {
+class Numbers extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {date: new Date()};
+        this.state = {numbers: 1};
     }
 
     componentDidMount() {
@@ -19,19 +19,19 @@ class Clock extends React.Component {
     }
     
     tick() {
-        this.setState({
-            date: new Date()
-        });
+        this.setState((state, props) => ({
+            numbers: state.numbers + 1
+        }));
     }
 
     render() {
         return (
             <div>
                 <h1>Hello, world!</h1>
-                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+                <h2>It is {this.state.numbers}.</h2>
             </div>
         );
     }
 }
 
-export default Clock;
+export default Numbers;
